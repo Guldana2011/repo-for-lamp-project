9be6aa0ee8b20f05eb4d86676e1cbc1f716fe788
Guldana2011 <guldeneagayeva01@gmail.com>
2025-04-07 22:32:54 +0400

    I added my lamp project

---
+++ app.js
@@ -0,0 +1,7 @@
+function LampOff() {
+    document.getElementById('lamp').src = "./images/pic_bulboff.gif"; 
+}
+
+function LampOn() {
+    document.getElementById('lamp').src = "./images/pic_bulbon.gif";
+}
